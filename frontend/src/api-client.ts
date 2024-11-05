74ca56dbab9a19665a07b849eb05bb1553603208
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-11-05 11:31:50 +0530

    fix shared folder

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,6 +1,6 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInData } from "./pages/Signin";
-import { HotelType } from "../../backend/src/models/hotel";
+import { HotelType } from "../../backend/src/shared/types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
