7266166e9406ae21286b17956f9801757a20ab76
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-11-08 00:09:08 +0530

    mybookings page

---
+++ backend/src/routes/my-bookings.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import verifyToken from "../middleware/auth";
+import Hotel from "../models/hotel";
+import { HotelType } from "../shared/types";
+const router = express.Router();
+
+router.get("/", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find({
+      "bookings.userId": req.userId,
+    });
+    const results = hotels.map((hotel) => {
+      const userBookings = hotel.bookings.filter(
+        (booking) => booking.userId == req.userId
+      );
+
+      const hotelWithUserBookings: HotelType = {
+        ...hotel.toObject(),
+        bookings: userBookings,
+      };
+      return hotelWithUserBookings;
+    });
+    res.status(200).send(results);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({
+      message: "Unable to fetch the hotels",
+    });
+  }
+});
+
+export default router;
